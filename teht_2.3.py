1985b1ee9cc7df7eff4c54b4f6e6c32725b96415
arturgola <artur.golavskiy@metropolia.fi>
2022-08-22 14:44:19 +0300

    commit

---
+++ teht_2.3.py
@@ -0,0 +1,12 @@
+import math
+#kysytän tiedot
+kanta = float(input("Anna suorakulmion kanta"))
+korkeus = float(input("Anna suorakulmion korkeus"))
+
+#laskutoimitukset
+A = kanta + korkeus
+piiri = (kanta * 2) + (korkeus * 2)
+
+#tulokset
+print(f"Ala = {A}")
+print(f"Piiri = {piiri}")
