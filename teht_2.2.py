2df8029c8ae89cf138e25420ca9f0df354021b22
arturgola <artur.golavskiy@metropolia.fi>
2022-08-22 14:32:50 +0300

    commit

---
+++ teht_2.2.py
@@ -0,0 +1,13 @@
+# Ohjelmaa laskee sourakulmaisen kolmion
+# pinta ala, kun kanta ja korkeus
+# annetaan.
+#piiri = (float)(input("Anna kolmion piiri"))
+#korkeus = (float)(input("Anna kolmion korkeus"))
+# ala = (kanta + korkeus) / 2
+#piiri = (piiri) / 2 * 3,1415
+#print("Kolmion ala on" + str(ala))
+
+import math
+r = float(input("Anna ympyrän säde"))
+ala = math.pi = math.pow(r,2)
+print(f"Ympyrän pinta-ala on {ala:.3f}")
