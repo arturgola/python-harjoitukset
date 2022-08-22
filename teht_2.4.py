fbebc11c1c00db7869a84e7c5f857433a626546e
arturgola <artur.golavskiy@metropolia.fi>
2022-08-22 14:55:05 +0300

    commit

--- teht_2.4.py
+++ teht_2.4.py
@@ -1,3 +1,11 @@
-luku_1_ = int(input("Anna kokonaisluku 1"))
-luku_2_ = int(input("Anna kokonaisluku 2"))
-luku_3_ = int(input("Anna kokonaisluku 3"))
+luku_1 = int(input("Anna kokonaisluku 1"))
+luku_2 = int(input("Anna kokonaisluku 2"))
+luku_3 = int(input("Anna kokonaisluku 3"))
+
+summa = luku_1 + luku_2 + luku_3
+tulo = luku_1 * luku_2 * luku_3
+keskiarvo = summa / 3
+
+print(summa)
+print(tulo)
+print(keskiarvo)
