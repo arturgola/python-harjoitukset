2456394c26bb685c4e689adc7ccbba0b105097ec
arturgola <artur.golavskiy@metropolia.fi>
2022-08-23 20:01:28 +0300

    commit

--- teht_2.5.py
+++ teht_2.5.py
@@ -1,10 +1,6 @@
-# Yksi leiviskä on 20 фунт.
-# Yksi фунт on 32 лот.
-# Yksi лот on 13,3 grammaa.
-
-lu1 = int(input("Anna leivisköintä"))
-lu2 = int(input("Anna nauloita"))
-lu3 = int(input("Anna luoteita"))
+lu1 = int(input("Anna leiviskät"))
+lu2 = int(input("Anna naulata"))
+lu3 = int(input("Anna luodit"))
 
 summa = (((lu1 * 20) + lu2) * 32 + lu3) * 13.3
 kg, grams = divmod(summa, 1000)
