15e74881ca8477e6a317f19008bd6c91a644c490
arturgola <artur.golavskiy@metropolia.fi>
2022-08-23 20:25:25 +0300

    commit

--- teht_2.6.py
+++ teht_2.6.py
@@ -1,4 +1,11 @@
 import random
 
 x = random.randint(0, 9)
-print (x)
+x1 = random.randint(0, 9)
+x2 = random.randint(0, 9)
+x3 = random.randint(0, 6)
+x4 = random.randint(0, 6)
+x5 = random.randint(0, 6)
+x6 = random.randint(0, 6)
+
+print(f"{x}{x1}{x2} {x3}{x4}{x5}{x6}")
