811c41bd64c1d9ac2a8bf227b7eeaac248267e9a
arturgola <arturgo@metropolia.fi>
2022-11-22 09:06:59 +0200

    ʕ•ﻌ•`ʔ

--- mod13/teht_13.1.py
+++ mod13/teht_13.1.py
@@ -1,15 +1,45 @@
-from flask import Flask
+from flask import Flask, Response
+import json
 
 app = Flask(__name__)
-@app.route("/primeNumber/<numeber>")
-def primeNumber(number):
-  num = int(number)
-  maxNum = round(num / 2)
-  for i in range(2, maxNum):
-    if num % i == 0:
-      return {"Number": num, "isPrime": False}
-  return {"Number": num, "isPrime": True}
 
 
-if __name__ == "__main__":
-  app.run(use_reloader=True, port=5000)
+@app.route('/alkuluku/<luku>')
+def alkuluku(num):
+    try:
+        num = int(num)
+        for i in range(2, num):
+            if num % i == 0:
+                result = False
+                break
+            else:
+                result = True
+        statusCode = 200
+        answer = {
+            "Number": num,
+            "isPrime": result,
+        }
+
+    except ValueError:
+        tilakoodi = 400
+        answer = {
+            "status": tilakoodi,
+            "teksti": "Virheellinen yhteenlaskettava"
+        }
+
+    jsonAnswer = json.dumps(answer)
+    return Response(response=jsonAnswer, status=statusCode, mimetype="application/json")
+
+
+@app.errorhandler(404)
+def page_not_found(errorCode):
+    answer = {
+        "status": "404",
+        "teksti": "Virheellinen päätepiste"
+    }
+    jsonAnswer = json.dumps(answer)
+    return Response(response=jsonAnswer, status=404, mimetype="application/json")
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=5000)
--- mod13/teht_13.2.py
+++ mod13/teht_13.2.py
@@ -2,50 +2,54 @@ from flask import Flask, Response
 import json
 import mysql.connector
 
-connection = mysql.connector.connect(
-         host='127.0.0.1',
-         port= 3306,
-         database='flight_game',
-         user='admin',
-         password="admin1",
-         autocommit=True
-         )
-
+yhteys = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user='admin',
+    password='admin1',
+    autocommit=True
+)
 app = Flask(__name__)
-@app.route('/kenttä/<Koodi>')
-def kentta(Koodi):
+@app.route('/kentta/<icao>')
+
+def kentta(icao):
     try:
-        sql = "select name, municipality from airport where ident = '" + Koodi +"' "
-        cursor = connection.cursor()
+        icao = str(icao)
+        sql ="SELECT name, municipality FROM airport WHERE ident = '" + icao + "'"
+        cursor = yhteys.cursor()
         cursor.execute(sql)
         result = cursor.fetchall()
-        Lentokenttä = (result[0][0])
-        Kaupunki = (result[0][1])
-        tilakoodi = 200
-        vastaus = {
-            "ICAO": Koodi,
-            "Name": Lentokenttä,
-            "Municipality": Kaupunki
+        for i in result:
+            name = i[0]
+            city = i[1]
+        statusCode = 200
+        answer = {
+            "ICAO": icao,
+            "Name": name,
+            "Municipality": city,
         }
 
     except ValueError:
-        tilakoodi = 400
-        vastaus = {
-            "status": tilakoodi,
-            "teksti": "Virheellinen yhteenlaskettava"
+        statusCode = 400
+        answer = {
+            "status": statusCode,
+            "text": "Virheellinen yhteenlaskettava"
         }
 
-    jsonvast = json.dumps(vastaus)
-    return Response(response=jsonvast, status=tilakoodi, mimetype="application/json")
+    jsonAnswer = json.dumps(answer)
+    return Response(response=jsonAnswer, status=statusCode, mimetype="application/json")
+
 
 @app.errorhandler(404)
-def page_not_found(virhekoodi):
-    vastaus = {
-        "status" : "404",
-        "teksti" : "Virheellinen päätepiste"
+def page_not_found(errorCode):
+    answer = {
+        "status": "404",
+        "teksti": "Virheellinen päätepiste"
     }
-    jsonvast = json.dumps(vastaus)
-    return Response(response=jsonvast, status=404, mimetype="application/json")
+    jsonAnswer = json.dumps(answer)
+    return Response(response=jsonAnswer, status=404, mimetype="application/json")
+
 
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=3000)
